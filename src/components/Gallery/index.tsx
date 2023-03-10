2ea687fb35c8adfc498306c62efd0baa05025d05
ValterMonteiro <valtinms@gmail.com>
2023-03-09 21:02:01 -0400

    💄 update component gallery

--- src/components/Gallery/index.tsx
+++ src/components/Gallery/index.tsx
@@ -1,21 +1,19 @@
 import './styles.css'
 
-import { ArrowCircleLeft, ArrowCircleRight } from 'phosphor-react';
+import { ArrowCircleRight } from 'phosphor-react';
 
 import { movies } from './data';
 import { useState } from 'react';
 
 export function Gallery() {
-    
+
     const [id, setId] = useState(0);
 
-    console.log('clicou');
-    
-    function handleClick() {
-        setId( id + 1);
-        //setMovie(event.target.value);
+    function handleClick() {        
+        (id < (movies.length -1))? setId(id + 1): setId(0);
         console.log(id);
     }
+
 
     const movie = movies[id];
 
@@ -23,8 +21,12 @@ export function Gallery() {
         <>
             <section className="gallery">
                 <img src={movie.images.poster} alt="" />
+                <details className='galleryDetails'>
+                <summary>Ver Detalhes!</summary>
+                <h1>{movie.title}</h1>
+                <p>{movie.synopsis}</p>
+                </details>
                 <div className='galleryArrow'>
-                    <ArrowCircleLeft size={32} onClick={handleClick} />
                     <ArrowCircleRight size={32} onClick={handleClick} />
                 </div>
             </section>
